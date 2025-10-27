2f183daa6d216822ede4b0eeafe70897be8a7fd2
BloodDonationBaguio <rcrossbaguio@gmail.com>
2025-10-27 20:28:04 +0800

    debug: print PHPMailer error directly in test_mail_debug.php for instant diagnosis

--- admin/test_mail_debug.php
+++ admin/test_mail_debug.php
@@ -8,8 +8,21 @@ $subject = 'Test Email from Blood Donation System';
 $message = '<h2>This is a test email from your Render deployment.</h2>';
 $result = false;
 if (function_exists('send_confirmation_email')) {
+    ob_start();
     $result = send_confirmation_email($to, $subject, $message, 'Test User');
-    echo $result ? '<span style="color:green;">SUCCESS: Email sent (check your inbox or spam).</span>' : '<span style="color:red;">FAIL: Email not sent. See logs/email_errors.log.</span>';
+    $debug = ob_get_clean();
+    if ($result) {
+        echo '<span style="color:green;">SUCCESS: Email sent (check your inbox or spam).</span>';
+    } else {
+        echo '<span style="color:red;">FAIL: Email not sent.<br>';
+        if (function_exists('error_get_last')) {
+            $err = error_get_last();
+            if ($err) echo 'PHP Error: ' . htmlspecialchars($err['message']) . '<br>';
+        }
+        echo 'If available, see logs/email_errors.log.<br>';
+        if (!empty($debug)) echo '<pre>' . htmlspecialchars($debug) . '</pre>';
+        echo '</span>';
+    }
 } else {
     echo '<span style="color:red;">send_confirmation_email() function not found.</span>';
 }
